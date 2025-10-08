eb7cfe5aaf079c69d09b617b7d8899d77f64ca4b
= <coderking201@gmail.com>
2025-10-08 16:22:57 +0530

    Lesson 10 Creating 'Enable Dark Mode' Button Using useState Hook

--- textUtils/src/App.jsx
+++ textUtils/src/App.jsx
@@ -5,6 +5,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/Navbar'
 import TextForm from './components/TextForm'
+import About from './components/About'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,6 +14,7 @@ function App() {
 <Navbar title={"TextUtils"} aboutText = "About TextUtils" />
 <div className="container my-3">
 <TextForm heading ="Enter the text to analyze below"/>
+{/* <About/> */}
 </div>
  </>
   )
