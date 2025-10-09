5bbb21ff46741fb65d8aedabdbb9168b6c6a069e
= <coderking201@gmail.com>
2025-10-09 15:35:47 +0530

    Lesson 11 Exercise 1: Solutions + Shoutout

--- textUtils/src/components/TextForm.jsx
+++ textUtils/src/components/TextForm.jsx
@@ -19,6 +19,16 @@ export default function TextForm(props) {
     const handleClear=()=>{
       setText('')
     }
+    const handleCopy = ()=>{
+      const text = document.getElementById("myBox")
+      text.select()
+      
+      navigator.clipboard.writeText(text.value)
+    }
+    const handleExtraSpaces = ()=>{
+      let newText = text.split(/[ ]+/)
+      setText(newText.join(" "))
+    }
     //text = "new text"// Wrong way to change the state
    // setText("new text")// Correct way to change the state
   return (
@@ -32,6 +42,8 @@ export default function TextForm(props) {
   <button className="btn btn-primary mx-1" onClick = {handleUpClick}>Convert To Uppercase</button>
   <button className="btn btn-primary mx-1" onClick = {handleLoClick}>Convert To Lowercase</button>
   <button className="btn btn-primary mx-1" onClick = {handleClear}>Clear Text</button>
+  <button className="btn btn-primary mx-1" onClick = {handleCopy}>Copy Text</button>
+  <button className="btn btn-primary mx-1" onClick = {handleExtraSpaces}>Handle Extra Spaces</button>
   </div>
   <div className="container my-3">
     <h2>Your text summary</h2>
