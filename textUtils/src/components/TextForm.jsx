f643118ae535b05c1fb2780f8598a027aff9a64b
= <coderking201@gmail.com>
2025-10-10 16:32:15 +0530

    Lesson 13 Improving Dark Mode & Refactoring App Component

--- textUtils/src/components/TextForm.jsx
+++ textUtils/src/components/TextForm.jsx
@@ -33,10 +33,11 @@ export default function TextForm(props) {
    // setText("new text")// Correct way to change the state
   return (
     <>
-    <div className='container'>
+    <div className='container' style={{color:props.mode==="light"?"#042743":"white"}}>
       <h1>{props.heading}</h1>
-<div className="mb-3">
-<textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8">
+<div className="mb-3" style={{color:props.mode==="light"?"#042743":"white"}}>
+<textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"
+style={{backgroundColor:props.mode==="light"?"white":"grey", color:props.mode==="light"?"black":"white"}}>
 </textarea>
   </div>
   <button className="btn btn-primary mx-1" onClick = {handleUpClick}>Convert To Uppercase</button>
@@ -45,12 +46,12 @@ export default function TextForm(props) {
   <button className="btn btn-primary mx-1" onClick = {handleCopy}>Copy Text</button>
   <button className="btn btn-primary mx-1" onClick = {handleExtraSpaces}>Handle Extra Spaces</button>
   </div>
-  <div className="container my-3">
+  <div className="container my-3" style={{color:props.mode==="light"?"#042743":"white"}}>
     <h2>Your text summary</h2>
     <p>{ text.split(" ").length } words, {text.length} characters</p>
     <p>{ 0.008 * text.split(" ").length } Minutes read</p>
     <h2>Preview</h2>
-    <p>{text}</p>
+    <p>{text.length === 0?text:"Enter something in the text box above to preview it here" }</p>
   </div>
   </>
   )
