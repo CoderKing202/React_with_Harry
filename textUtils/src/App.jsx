16f499bb2656bcd1c7b24d12aa6604045dee4193
= <coderking201@gmail.com>
2025-10-13 12:18:33 +0530

    Lesson 15 Changing title dynamically & adding favicons to TextUtils

--- textUtils/src/App.jsx
+++ textUtils/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import viteLogo from '/favicon.ico'
 
 import './App.css'
 import Navbar from './components/Navbar'
@@ -10,7 +10,6 @@ import Alert from './components/Alert'
 
 function App() {
   const [mode,setMode] = useState('light')//Whether dark mode is enabled or not
-<<<<<<< HEAD
   const [alert, setAlert] = useState(null)
   const showAlert = (message, type)=>{
     setAlert({message,type})
@@ -19,39 +18,35 @@ function App() {
     },1500)
   }
 
-=======
->>>>>>> f643118ae535b05c1fb2780f8598a027aff9a64b
   const toggleMode = ()=>{
   if(mode === "light"){
     setMode("dark")
     document.body.style.backgroundColor="#042743"
-<<<<<<< HEAD
+    document.title = "TextUtils - Dark Mode"
+    // setInterval(()=>{
+    //   document.title = "TextUtils is amazing"
+    // },2000)
+    // setInterval(()=>{
+    //   document.title = "Install TextUtils now"
+    // },1500)
     showAlert("Dark mode has been enabled", "success")
-=======
->>>>>>> f643118ae535b05c1fb2780f8598a027aff9a64b
+
   } 
   else{
     setMode("light")
     document.body.style.backgroundColor="white"
-<<<<<<< HEAD
+    document.title = "TextUtils - Light Mode"
     showAlert("Light mode has been enabled", "success")
-=======
->>>>>>> f643118ae535b05c1fb2780f8598a027aff9a64b
   } 
   }
 
   return (
  <>
 <Navbar title={"TextUtils"} mode={mode} toggleMode={toggleMode} />
-<<<<<<< HEAD
 <Alert alert={alert}/>
 <div className="container my-3">
 <TextForm heading ="Enter the text to analyze below" mode={mode} 
 showAlert={showAlert}/>
-=======
-<div className="container my-3">
-<TextForm heading ="Enter the text to analyze below" mode={mode}/>
->>>>>>> f643118ae535b05c1fb2780f8598a027aff9a64b
 {/* <About/> */}
 </div>
  </>
