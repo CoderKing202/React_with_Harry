ac4d3f86ff86ae78cce0d1ab962c1d75d129fe5f
CoderKing202 <168006412+CoderKing202@users.noreply.github.com>
2025-10-24 16:56:13 +0530

    Lesson 28 Adding Previous & Next Buttons to populate NewsItems

--- textUtils/src/components/News.jsx
+++ textUtils/src/components/News.jsx
@@ -5,14 +5,37 @@ import { useState } from 'react'
 function News() {
   
   useEffect( ()=>{(async ()=>{
-    let url = "https://newsapi.org/v2/top-headlines?q=trump&apiKey=28df2a883ac44eedb7ef9cdac2329fa1"
+    let url = "https://newsapi.org/v2/top-headlines?q=trump&apiKey=28df2a883ac44eedb7ef9cdac2329fa1&page=1&pageSize=20"
     let data = await fetch(url)  
     let parsedData = await data.json()
+    setTotalResult(parsedData.totalResults)
     setNews(parsedData.articles)
   })()
 },[])
- 
+const handlePrevClick = async ()=>{
+    let url = `https://newsapi.org/v2/top-headlines?q=trump&apiKey=28df2a883ac44eedb7ef9cdac2329fa1&page=${page - 1}&pageSize=20`
+    let data = await fetch(url)  
+    let parsedData = await data.json()
+    setNews(parsedData.articles)
+    setPage( page - 1 )
+} 
+const handleNextClick = async ()=>{
+    if(page + 1 > Math.ceil(totalResult/20)) 
+    {
+  }
+  else{
+    let url = `https://newsapi.org/v2/top-headlines?q=trump&apiKey=28df2a883ac44eedb7ef9cdac2329fa1&page=${page + 1}&pageSize=20`
+    let data = await fetch(url)  
+    let parsedData = await data.json()
+    setNews(parsedData.articles)
+    setPage( page + 1 )
+  }
+
+}
+  const [page,setPage] = useState(1)
   const [news,setNews] = useState([])
+  const [totalResult,setTotalResult] = useState(0)
+
   return<div className="container my-3">
       <h1>NewsMonkey - Top Headlines</h1>
       <div className="row">
@@ -23,7 +46,10 @@ function News() {
         newsUrl = {element.url}/>
         </div>
       })}
-        
+      </div>
+      <div class="container d-flex justify-content-between">
+      <button disabled={page<=1} type="button" className="btn btn-dark" onClick={handlePrevClick}> &larr; Previous</button>
+      <button type="button" className="btn btn-dark" onClick={handleNextClick}>Next &rarr;</button>
       </div>
     </div>
     }
