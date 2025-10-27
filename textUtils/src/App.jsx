257c6325c531763960be150594a65dc61e6a4d30
CoderKing202 <168006412+CoderKing202@users.noreply.github.com>
2025-10-27 17:09:47 +0530

    Lesson 31 Fetching News category wise in NewsMonkey React App

--- textUtils/src/App.jsx
+++ textUtils/src/App.jsx
@@ -2,14 +2,38 @@ import './App.css'
 import React, { Component } from 'react'
 import NavBar from './components/Navbar'
 import News from './components/News'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
 //Teacher is doing in class way but i will do it in functional component way
 function App() {
   return (
     <div>
+      <Router>
       <NavBar/>
-      <News/>
+      <Routes>
+      <Route path="/"element={<News key="general" pageSize={5} category="general" country="us"/>}></Route>
+      <Route path="/business"element={<News key="business" pageSize={5} category="business" country="us"/>}></Route>
+      <Route path="/entertainment"element={<News key="entertainment" pageSize={5} category="entertainment" country="us"/>}></Route>
+      <Route path="/health"element={<News key="health" pageSize={5} category="health" country="us"/>}></Route>
+      <Route path="/science"element={<News key="science" pageSize={5} category="science" country="us"/>}></Route>
+      <Route path="/sports"element={<News key="sports" pageSize={5} category="sports" country="us"/>}></Route>
+      <Route path="/technology"element={<News key="technology" pageSize={5} category="technology" country="us"/>}></Route>
+<Route path="/"element={<News key="story" pageSize={5} category="sports" country="us"/>}>
+</Route>
+
+      </Routes>
+      </Router>
     </div>
+    
   )
 }
-
+/* 
+business
+entertainment
+general
+health
+science
+sports
+technology
+*/
 export default App
--- textUtils/src/components/News.jsx
+++ textUtils/src/components/News.jsx
@@ -1,45 +1,53 @@
 import React, { useEffect } from 'react'
 import NewsItem from './NewsItem'
 import { useState } from 'react'
+import Spinner from './Spinner'
 
-function News() {
-  
+function News(props) {
+  const {pageSize, country="us",category="science"} = props// default values in destruction
   useEffect( ()=>{(async ()=>{
-    let url = "https://newsapi.org/v2/top-headlines?q=trump&apiKey=28df2a883ac44eedb7ef9cdac2329fa1&page=1&pageSize=20"
+    let url = `https://newsapi.org/v2/top-headlines?category=${category}&apiKey=28df2a883ac44eedb7ef9cdac2329fa1&country=${country}&page=1&pageSize=${pageSize}`
+    setLoading(true)
     let data = await fetch(url)  
     let parsedData = await data.json()
     setTotalResult(parsedData.totalResults)
     setNews(parsedData.articles)
+    setLoading(false)
   })()
 },[])
 const handlePrevClick = async ()=>{
-    let url = `https://newsapi.org/v2/top-headlines?q=trump&apiKey=28df2a883ac44eedb7ef9cdac2329fa1&page=${page - 1}&pageSize=20`
+    let url = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=28df2a883ac44eedb7ef9cdac2329fa1&page=${page - 1}&pageSize=${pageSize}`
+    setLoading( true )
     let data = await fetch(url)  
     let parsedData = await data.json()
     setNews(parsedData.articles)
     setPage( page - 1 )
+    setLoading( false )
 } 
 const handleNextClick = async ()=>{
-    if(page + 1 > Math.ceil(totalResult/20)) 
+    if( !(page + 1 >= Math.ceil(totalResult/pageSize)) ) 
     {
-  }
-  else{
-    let url = `https://newsapi.org/v2/top-headlines?q=trump&apiKey=28df2a883ac44eedb7ef9cdac2329fa1&page=${page + 1}&pageSize=20`
+    setLoading(true)
+    let url = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=28df2a883ac44eedb7ef9cdac2329fa1&page=${page + 1}&pageSize=${pageSize}`
     let data = await fetch(url)  
     let parsedData = await data.json()
-    setNews(parsedData.articles)
+    setNews(parsedData.articles) 
     setPage( page + 1 )
+    setLoading(false)
+    console.log(parsedData.articles)
   }
 
 }
+  const [loading, setLoading] = useState(false)
   const [page,setPage] = useState(1)
   const [news,setNews] = useState([])
   const [totalResult,setTotalResult] = useState(0)
 
   return<div className="container my-3">
-      <h1>NewsMonkey - Top Headlines</h1>
+      <h1 className='text-center' style={{margin:'35px 0px'}}>NewsMonkey - Top Headlines</h1>
+      { loading && <Spinner/>}
       <div className="row">
-      {news.map((element)=>{
+      {!loading && news.map((element)=>{
         return <div className="col md-3" key={element.url}>
         <NewsItem  title={element.title} description={element.description} 
         imageUrl={element.urlToImage}
@@ -47,10 +55,11 @@ const handleNextClick = async ()=>{
         </div>
       })}
       </div>
-      <div class="container d-flex justify-content-between">
+      <div className="container d-flex justify-content-between">
       <button disabled={page<=1} type="button" className="btn btn-dark" onClick={handlePrevClick}> &larr; Previous</button>
-      <button type="button" className="btn btn-dark" onClick={handleNextClick}>Next &rarr;</button>
+      <button disabled={page + 1 >= Math.ceil(totalResult/pageSize)} type="button" className="btn btn-dark" onClick={handleNextClick}>Next &rarr;</button>
       </div>
+      
     </div>
     }
 
