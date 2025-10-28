c2cc456836b37a5ebf472f4f2fb72f3db10fd045
CoderKing202 <168006412+CoderKing202@users.noreply.github.com>
2025-10-28 15:46:30 +0530

    Lesson 33 Refactoring News component to use the same function

--- textUtils/src/App.jsx
+++ textUtils/src/App.jsx
@@ -6,21 +6,20 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 //Teacher is doing in class way but i will do it in functional component way
 function App() {
+const pageSize = 15
+
   return (
     <div>
       <Router>
       <NavBar/>
       <Routes>
-      <Route path="/"element={<News key="general" pageSize={5} category="general" country="us"/>}></Route>
-      <Route path="/business"element={<News key="business" pageSize={5} category="business" country="us"/>}></Route>
-      <Route path="/entertainment"element={<News key="entertainment" pageSize={5} category="entertainment" country="us"/>}></Route>
-      <Route path="/health"element={<News key="health" pageSize={5} category="health" country="us"/>}></Route>
-      <Route path="/science"element={<News key="science" pageSize={5} category="science" country="us"/>}></Route>
-      <Route path="/sports"element={<News key="sports" pageSize={5} category="sports" country="us"/>}></Route>
-      <Route path="/technology"element={<News key="technology" pageSize={5} category="technology" country="us"/>}></Route>
-<Route path="/"element={<News key="story" pageSize={5} category="sports" country="us"/>}>
-</Route>
-
+      <Route path="/"element={<News key="general" pageSize={pageSize} category="general" country="us"/>}></Route>
+      <Route path="/business"element={<News key="business" pageSize={pageSize} category="business" country="us"/>}></Route>
+      <Route path="/entertainment"element={<News key="entertainment" pageSize={pageSize} category="entertainment" country="us"/>}></Route>
+      <Route path="/health"element={<News key="health" pageSize={pageSize} category="health" country="us"/>}></Route>
+      <Route path="/science"element={<News key="science" pageSize={pageSize} category="science" country="us"/>}></Route>
+      <Route path="/sports"element={<News key="sports" pageSize={pageSize} category="sports" country="us"/>}></Route>
+      <Route path="/technology"element={<News key="technology" pageSize={pageSize} category="technology" country="us"/>}></Route>
       </Routes>
       </Router>
     </div>
--- textUtils/src/components/News.jsx
+++ textUtils/src/components/News.jsx
@@ -4,44 +4,39 @@ import { useState } from 'react'
 import Spinner from './Spinner'
 
 function News(props) {
-  const {pageSize, country="us",category="science"} = props// default values in destruction
-  useEffect( ()=>{(async ()=>{
-    let url = `https://newsapi.org/v2/top-headlines?category=${category}&apiKey=28df2a883ac44eedb7ef9cdac2329fa1&country=${country}&page=1&pageSize=${pageSize}`
-    setLoading(true)
-    let data = await fetch(url)  
-    let parsedData = await data.json()
-    setTotalResult(parsedData.totalResults)
-    setNews(parsedData.articles)
-    setLoading(false)
-  })()
-},[])
-const handlePrevClick = async ()=>{
-    let url = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=28df2a883ac44eedb7ef9cdac2329fa1&page=${page - 1}&pageSize=${pageSize}`
-    setLoading( true )
-    let data = await fetch(url)  
-    let parsedData = await data.json()
-    setNews(parsedData.articles)
-    setPage( page - 1 )
-    setLoading( false )
-} 
-const handleNextClick = async ()=>{
-    if( !(page + 1 >= Math.ceil(totalResult/pageSize)) ) 
-    {
-    setLoading(true)
-    let url = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=28df2a883ac44eedb7ef9cdac2329fa1&page=${page + 1}&pageSize=${pageSize}`
-    let data = await fetch(url)  
-    let parsedData = await data.json()
-    setNews(parsedData.articles) 
-    setPage( page + 1 )
-    setLoading(false)
-    console.log(parsedData.articles)
-  }
-
-}
   const [loading, setLoading] = useState(false)
   const [page,setPage] = useState(1)
   const [news,setNews] = useState([])
   const [totalResult,setTotalResult] = useState(0)
+  const {pageSize, country="us",category="science"} = props// default values in destruction
+  useEffect( ()=>{
+  updateNews()
+  
+},[page])
+const updateNews = async ()=>{
+  
+  const  url = `https://newsapi.org/v2/top-headlines?category=${category}&apiKey=28df2a883ac44eedb7ef9cdac2329fa1&country=${country}&page=${page}&pageSize=${pageSize}`
+  setLoading(true)
+  let data = await fetch(url)  
+  let parsedData = await data.json()
+  setTotalResult(parsedData.totalResults)
+  setNews(parsedData.articles)
+  setLoading(false)
+  console.log(page)
+}
+const handlePrevClick = async ()=>{
+    setPage( page - 1 )
+    
+} 
+const handleNextClick = async ()=>{
+    if( !(page + 1 >= Math.ceil(totalResult/pageSize)) ) 
+    {
+    setPage( page + 1 )
+    
+  }
+
+}
+
 
   return<div className="container my-3">
       <h1 className='text-center' style={{margin:'35px 0px'}}>NewsMonkey - Top Headlines</h1>
