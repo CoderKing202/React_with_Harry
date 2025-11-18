08a73ae2a2e6cf81624bc4100a9a64477a56de86
CoderKing202 <168006412+CoderKing202@users.noreply.github.com>
2025-11-18 09:43:39 +0530

    Lesson 50 Creating Login Endpoint & sending auth token

--- inotebook/backend/routes/auth.js
+++ inotebook/backend/routes/auth.js
@@ -22,7 +22,7 @@ router.post("/createuser", [
   try{
   let user = await User.findOne({email:req.body.email})
   if(user){
-    return res.status(400).json({error:"Sorry a user with this email already exists"})
+    return res.status(400).json({error:"Please try to login with correct Credentials"})
   }
   const salt = await bcrypt.genSalt(10)
   const secPass = await bcrypt.hash(req.body.password,salt)
@@ -46,7 +46,7 @@ router.post("/createuser", [
 //catch errors
   catch(error){
     console.error(error.message)
-    res.status(500).send("Some error occured")
+    res.status(500).send("Internal Server Error")
   }
   
   // .then(user=> res.json(user))
@@ -58,4 +58,39 @@ router.post("/createuser", [
   // res.send(req.body)
 });
 
+// Authenticate a User using: POST "/api/auth/login" No login required
+router.post("/login", [
+   body("email","Enter a valid email").isEmail(),
+   body("password","Password cannot be blank").exists(),//check if the password is not empty
+],async (req, res) => {
+  // if there are errors return bad request and the errors
+  const errors = validationResult(req)
+  if(!errors.isEmpty())
+  {
+    return res.status(400).json({errors:errors.array()})
+  }
+  const {email, password} = req.body
+  try {
+    let user = await User.findOne({email})
+    if (!user)
+    {
+      return res.status(400).json({error:"Please try to login with correct credentials"})
+    }
+    const passwordCompare = await bcrypt.compare(password,user.password)
+    if(!passwordCompare)
+    {
+      return res.status(400).json({error:"Please try to login with correct credentials"})
+    }
+    
+    const payload = {
+      user:{
+        id : user.id
+      }
+    }
+    const authtoken = jwt.sign(payload,JWT_SECRET)
+    res.json({authtoken})
+  } catch (error) {
+   console.error(error.message)
+    res.status(500).send("Internal Server Error")  }
+})
 module.exports = router;
