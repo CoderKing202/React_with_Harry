e40681240119e804831a3d8008185a68da74ed07
CoderKing202 <168006412+CoderKing202@users.noreply.github.com>
2025-11-24 14:54:13 +0530

    Lesson 54 Endpoint for deleting a Note

--- inotebook/backend/routes/notes.js
+++ inotebook/backend/routes/notes.js
@@ -38,13 +38,14 @@ router.post(
     }
   }
 );
-// Route 3: Update an existing note using: POST "/api/notes/updatenote".Login Required
+// Route 3: Update an existing note using: PUT "/api/notes/updatenote".Login Required
 router.put(
   "/updatenote/:id",
   fetchuser,
   async (req, res) => {
    const {title, description, tag} = req.body
    // Create a new Note object
+   try{
    let newNote = {}
    if(title){ newNote.title = title}
    if(description){ newNote.description = description}
@@ -62,6 +63,39 @@ router.put(
  note = await Note.findByIdAndUpdate(req.params.id,{$set: newNote},{new:true})
  /** new:true => return object after update rather than before update */
  res.json({note})
+}
+catch(error){
+ console.error(error.message);
+ res.status(500).send("Internal Server Error");
+}
   } 
 );
+
+// Route 4: Delete an existing note using: DELETE "/api/notes/deletenote".Login Required
+router.delete(
+  "/deletenote/:id",
+  fetchuser,
+  async (req, res) => {
+ try{
+
+   // Find the note to be deleted and delete it
+   let note = await Note.findById(req.params.id)
+   if(!note){
+   return res.status(404).send("Not Found")
+  }
+  //Allow deletion only if user owns this Note 
+  if(note.user.toString() !== req.user.id)
+  {
+    return res.status(401).send("Not Allowed")
+  }
+ note = await Note.findByIdAndDelete(req.params.id)
+ /** new:true => return object after update rather than before update */
+ res.json({Success:"Note has been deleted",note})
+ }catch(error){
+  console.error(error.message);
+  res.status(500).send("Internal Server Error");
+ }  
+} 
+);
+
 module.exports = router;
