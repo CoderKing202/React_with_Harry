ae73dbaac4bf951732bce61b3bd2879eaaa97716
CoderKing202 <168006412+CoderKing202@users.noreply.github.com>
2025-11-25 19:54:23 +0530

    Lesson 56 Creating Navbar and Routes

--- inotebook/src/App.jsx
+++ inotebook/src/App.jsx
@@ -2,15 +2,24 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import NavBar from './components/NavBar'
+import {BrowserRouter as Router, Routes,Route} from "react-router-dom"
+import Home from './components/Home'
+import About from './components/About'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className="App">
-          <h1>This is iNoteBook</h1>
-      </div>
+      <Router>
+          <NavBar/>
+          <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/about" element={<About/>}/>
+      </Routes>
+      </Router>
     </>
   )
 }
